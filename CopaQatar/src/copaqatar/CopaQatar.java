cd5bdc85921cc7c2efe54ddf45d5f90b88092d5f
Thiago Costa <116419454+thiagocosta031083@users.noreply.github.com>
2022-12-21 02:12:59 -0300

    Add files via upload

---
+++ CopaQatar/src/copaqatar/CopaQatar.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package copaqatar;
+
+/**
+ *
+ * @author Thiago
+ */
+public class CopaQatar {
+
+    /**
+     * Para fazer comentários
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here 
+      SemiFinal confrontos = new SemiFinal();
+      System.out.println("Jogos da semi final: "+confrontos.timeA+" x "+ confrontos.timeC+
+              "\n  e " +confrontos.timeB+" x "+confrontos.timeD);//Para as 2 classes se comunicaraem tem que estar dentro do pacote
+      //Importante respeitar maiusculas e minusculas
+     /*
+      *Concatenar = +
+      
+      */
+      
+        
+    }
+    
+}
---
+++ CopaQatar/src/copaqatar/SemiFinal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package copaqatar;
+
+/**
+ *
+ * @author Thiago
+ */
+public class SemiFinal {
+     public String timeA="Argentina",timeB="França",timeC="Croacia", timeD="Marrocos";
+     
+}
